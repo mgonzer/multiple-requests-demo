7811376e4923655c77065ccd6b10592684d9eefe
Roberto Ortega <berto.ort@gmail.com>
2017-05-12 10:21:46 -0600

    ✨ Add helper function and cdns

--- api.js
+++ api.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   console.log("ready to go!");
 });
+
+function generateCard(imageUrl, question, answer) {
+  let cardTemplate = `
+    <div class="card">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="${imageUrl}">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">${question}<i class="material-icons right">more_vert</i></span>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">${question}<i class="material-icons right">close</i></span>
+        <p>${answer}</p>
+      </div>
+    </div>
+   `
+  $('main').append(cardTemplate);
+}
+
