ba05f1863ba32eed0123a9cf9e21bd29eea523bc
Matthew Gonzer <matthewgonzer@gmail.com>
2017-07-10 15:19:19 -0600

    multiple requests

--- api.js
+++ api.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
   console.log("ready to go!");
+  var question, answer;
+  var questions = [];
+  $.get('https://opentdb.com/api.php?amount=10&category=15&difficulty=easy').then(function(data) {
+    question = data.results[0].question;
+    answer =  data.results[0].correct_answer;
+    for (var i = 0, len = data.results.length; i < len; i++) {
+      var question = data.results[i].question;
+      var answer =  data.results[i].correct_answer;
+      questions.push({answer: answer, question: question});
+    }
+    var promises = []
+    for (var i = 0; i < questions.length ; i++) {
+      promises.push($.get('http://api.giphy.com/v1/gifs/search?q='+ questions[i].answer + '&api_key=dc6zaTOxFJmzC'))
+    }
+    return Promise.all(promises);
+  }).then(function(gifs) {
+     for (var i = 0; i < gifs.length; i++) {
+       generateCard(gifs[i].data[0].images.original.url, questions[i].question, questions[i].answer);
+     }
+  });
 });
 
 function generateCard(imageUrl, question, answer) {
@@ -19,4 +39,3 @@ function generateCard(imageUrl, question, answer) {
    `
   $('main').append(cardTemplate);
 }
-
